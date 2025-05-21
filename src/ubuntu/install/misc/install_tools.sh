555fddf38aedac988ff8298c10d91b8781684448
alacava <me@anthonylacava.com>
2025-05-21 13:30:26 -0400

    Added htop

--- src/ubuntu/install/misc/install_tools.sh
+++ src/ubuntu/install/misc/install_tools.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if [ "$DISTRO" = centos ]; then
-  yum install -y nano zip wget
+  yum install -y nano zip wget htop
   yum install epel-release -y
   yum install xdotool -y
 else
   apt-get update
-  apt-get install -y nano zip xdotool
+  apt-get install -y nano zip xdotool htop
 fi
